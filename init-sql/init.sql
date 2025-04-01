ad916087ff1e7f74e9d730c5605cf4fe663d9afc
mimoimio <129141433+mimoimio@users.noreply.github.com>
2025-04-01 10:05:25 +0000

    fixed db initialization

--- init-sql/init.sql
+++ init-sql/init.sql
@@ -3,13 +3,13 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 CREATE TABLE `answers` (
-  `id` int(11) NOT NULL,
+  `answer_id` int(11) NOT NULL,
   `answer_text` text NOT NULL,
   `user_id` int(11) NOT NULL,
   `time_created` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO `answers` (`id`, `answer_text`, `user_id`, `time_created`) VALUES
+INSERT INTO `answers` (`answer_id`, `answer_text`, `user_id`, `time_created`) VALUES
 (34, 'Hello\r\n', 32, '2025-02-26 15:33:54'),
 (35, 'Um Hello, hello? The animatronics do get a bit quirky at night', 30, '2025-02-26 15:38:42'),
 (36, '12312312312381u3971923210301238109238', 30, '2025-02-26 15:39:51'),
@@ -27,7 +27,7 @@ INSERT INTO `answers` (`id`, `answer_text`, `user_id`, `time_created`) VALUES
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `background` VARCHAR(50) NOT NULL DEFAULT 'white',
@@ -38,7 +38,7 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `background`, `time_created`) VALUES
+INSERT INTO `users` (`user_id`, `username`, `password`, `background`, `time_created`) VALUES
 (30, 'Mior', '$2y$10$Zyo5RBS.MuS2e2ZlBXXzjOPuL464FMViMLyPR/D8Clg2BUTCt41aW', 'white', '2025-02-26 08:10:42'),
 (31, 'Adib', '$2y$10$hU3/NBBS8oVccvcI3nW2SOeCEzJO5BxKcaiHc3v0OQQQjs8HagDT6', 'white', '2025-02-26 08:10:58'),
 (32, 'lol', '$2y$10$csqyshtx3wEA5FILizTi5uurNQJROQrKJ8vYr.0uy84JK3OAYNNOC', 'white', '2025-02-26 15:00:58'),
@@ -52,13 +52,13 @@ INSERT INTO `users` (`id`, `username`, `password`, `background`, `time_created`)
 -- Indexes for table `answers`
 --
 ALTER TABLE `answers`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`answer_id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`user_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -68,13 +68,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `answers`
 --
 ALTER TABLE `answers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+  MODIFY `answer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 COMMIT;
 
 -- /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
