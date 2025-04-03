954fbee80bca350c8693383c5c265d1d783b12e6
mimoimio <129141433+mimoimio@users.noreply.github.com>
2025-04-03 13:24:05 +0800

    htmlspecialchar on answer card

--- app/views/templates/answer_card.php
+++ app/views/templates/answer_card.php
@@ -2,9 +2,10 @@
     <a href="/answer/<?= $data['answer_id'] ?>" class="text-decoration-none">
         <div class="card h-100">
             <div class="card-body">
-                <h5 class="card-title text-truncate"><?= $data["answer_text"] ?></h5>
-                <p class="card-text" style="display: -webkit-box; -webkit-line-clamp: 3; -webkit-box-orient: vertical; overflow: hidden;"><?= $data["answer_text"] ?></p>
-                <p class="card-text">By <?= $data["username"] ?></p>
+                <h5 class="card-title text-truncate"><?= htmlspecialchars($data["answer_text"]) ?></h5>
+                <p class="card-text" style="display: -webkit-box; -webkit-line-clamp: 3; -webkit-box-orient: vertical; overflow: hidden;"><?= /*htmlspecialchars($data["answer_text"])*/ "" ?></p>
+                <p class="card-text" style="display: -webkit-box; -webkit-line-clamp: 3; -webkit-box-orient: vertical; overflow: hidden;"><?= htmlspecialchars($data["answer_text"]) ?></p>
+                <p class="card-text">By <?= htmlspecialchars($data["username"]) ?></p>
                 <h6 class="card-subtitle mb-2 text-muted"><?= $data["time_created"] ?></h6>
             </div>
         </div>
